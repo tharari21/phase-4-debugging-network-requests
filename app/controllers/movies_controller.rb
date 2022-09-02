2d5fb0d50b48a6089e37e1b7182b28c2dede6151
Tomer Harari <tharari21@gmail.com>
2022-09-02 17:46:26 -0400

    commit tree 153c76960d17157024f7e2fe38e9997ac88bac93

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,18 @@
 class MoviesController < ApplicationController
-  
+  def create
+    movie = Movie.create(movie_params)
+    if movie.persisted?
+      render json: movie
+    else
+      render json: {error: movie.errors.full_messages}
+    end
+  end
   def index
     movies = Movie.all
     render json: movies
   end
-
+  private
+  def movie_params
+    params.permit(:title,:year,:length,:director,:description,:poster_url,:category,:discount,:female_director)
+  end
 end
